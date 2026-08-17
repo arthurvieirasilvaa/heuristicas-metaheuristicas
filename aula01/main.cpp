0347ce8a375ba965b3026ad9f3a3e0b308c33b2d
Arthur Vieira Silva <arthursilvavieira03@gmail.com>
2026-08-17 16:29:34 -0300

    Implementei a representação utilizando Matriz de Adjacência

--- aula01/main.cpp
+++ aula01/main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <string>
 #include <fstream>
 #include <sstream>
+#include <vector>
 
 using namespace std;
+
+void matrixAddEdge(vector<vector<int>> &matrix, int v1, int v2, int weight) {
+    matrix[v1][v2] = weight;
+    matrix[v2][v1] = weight;
+}
+
+void printMatrix(vector<vector<int>> matrix) {
+    cout << "Matrix de Adjacencia:" << endl;
+    int size = matrix.size();
+
+    for(int i = 0; i < size; i++) {
+        for(int j = 0; j < size; j++) {
+            if(matrix[i][j] != -1) {
+                cout << i+1 << " -> " << j+1 << " : " << matrix[i][j] << endl;
+            } 
+        }
+    }
+}
 
 int main(int argc, char* argv[]) {
     if(argc < 2) {
@@ -38,9 +58,28 @@ int main(int argc, char* argv[]) {
             cout << "A linha do arquivo de entrada está corrompida!" << endl;
             exit(1);
         }
+
+        // Inicializa a Matriz de Adjacência com pesos -1:
+        vector<vector<int>> matrix(nodes, vector<int>(nodes, -1));
+        while(getline(file, line)) {
+            stringstream ss(line);
+
+            // Fazendo o parsing do stream de string:
+            int v1, v2, weight;
+            if(ss >> v1 >> v2 >> weight) {
+                matrixAddEdge(matrix, v1-1, v2-1, weight);
+            }
+            
+            else {
+                cout << "A linha do arquivo de entrada está corrompida!" << endl;
+                exit(1);
+            }
+        }
+
+        printMatrix(matrix);
     }
 
     file.close();
-    
+
     return 0;
 }
